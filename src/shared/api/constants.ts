990039081d9e90f66b123afaaafc08179aa25115
Volodymyr Pestov <pestov.v@yahoo.com>
2026-01-24 18:21:30 +0200

    changed api url

--- src/shared/api/constants.ts
+++ src/shared/api/constants.ts
@@ -1,2 +1,2 @@
-// export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001/api";
-export const API_URL = "https://candidates-api-production.up.railway.app/api";
+export const API_URL = import.meta.env.VITE_API_URL;
+// export const API_URL = "https://candidates-api-production.up.railway.app/api";
